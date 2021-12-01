93f0ceb037955694c709ae5a133e5e92683fc343
Samuel Barbosa <56175266+sml07@users.noreply.github.com>
2021-12-01 19:45:20 -0300

    a

---
+++ site/scripts/main.js
@@ -0,0 +1,6 @@
+sessionStorage.descricao = document.getElementById('descricao').innerHTML
+sessionStorage.valor = document.getElementById('valor').innerHTML
+
+function resumoCompra() {
+  window.location = 'cart.html'
+}
